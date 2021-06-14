5b59c988f34cd2602011f237021a9a38e02ca4fe
Jacob Pond <jpond@uwm.edu>
2021-06-14 15:21:45 -0500

    project images and resume

--- src/Website/urls.py
+++ src/Website/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home.as_view()),
     path('projects/', Projects.as_view()),
+    path('resume/', Resume.as_view()),
 ]
--- src/app1/views.py
+++ src/app1/views.py
@@ -10,11 +10,16 @@ class Home(View):
     def post(self, request):
         return render(request, "home.html", {})
 
-
-
 class Projects(View):
     def get(self, request):
         return render(request, "projects.html", {})
 
     def post(self, request):
         return render(request, "projects.html", {})
+
+class Resume(View):
+    def get(self, request):
+        return render(request, "resume.html", {})
+
+    def post(self, request):
+        return render(request, "resume.html", {})
